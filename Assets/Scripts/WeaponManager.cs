276005862b4090f4b08e10ff3c1de834d95521c8
송신화 <bruno0629@naver.com>
2025-04-13 00:22:41 +0900

    fix

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WeaponManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform weaponHolder;
+    private Weapon currentWeapon;
+    [SerializeField] GameObject weaponPrefab;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 테스트용 (추후 수정)
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            EquipWeapon(weaponPrefab);
+            Debug.Log("무기 변경");
+        }
+    } 
+
+    public void EquipWeapon(GameObject weaponPrefab)
+    {
+        foreach(Transform child in weaponHolder)
+        {
+            Destroy(child.gameObject);
+        }
+
+        GameObject newWeapon = Instantiate(weaponPrefab, weaponHolder);
+        newWeapon.transform.localPosition = Vector3.zero;
+        newWeapon.transform.localRotation = Quaternion.identity;
+
+        currentWeapon = newWeapon.GetComponent<Weapon>();
+        if(currentWeapon == null)
+        {
+            Debug.LogWarning("무기 프리팹에 Weapon 스크립트 없음");
+        }
     }
 }
